9ca188c93a1f2814cb8a0fa7b2a2bab287ac0788
MishaShekhar20 <mishashekhar7@gmail.com>
2025-05-28 19:55:20 +0530

    Arrays

---
+++ Reverse/src/Sports.java
@@ -0,0 +1,10 @@
+public class Sports {
+    public static void main(String[] args) {
+        String[] sports = {"Football", "Basketball", "Cricket", "Tennis", "Badminton", "Volleyball","Swimming", "Cycling","Hockey","Skating"};
+        for(int i = 0 ; i<sports.length ; i++){
+            System.out.println(sports[i]);
+        }
+
+
+    }
+}
